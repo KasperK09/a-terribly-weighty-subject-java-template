66753e861ab5b4595287a1de41a49d9b9ccf66ad
Kasper <93494353+KasperK09@users.noreply.github.com>
2026-09-04 22:03:31 -0500

    Converter done?

--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,36 +1,16 @@
 package org.example;
 
 public class Converter {    
-  int ounces;
 
-  System.out.println("How Many ounces do you have?");
-  int pounds = console.readLine
+  public double toPounds(int ounces) {
+    double pounds = ounces / 16.0;
 
-  pounds = ounces/16;
+    return pounds;
+  }
 
-  
+  public double toPoundsAndOunces(int ounces) {
+    int remaingOunces = ounces % 16;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // Write the code needed to make the tests in
-  // TestConverter.java pass. I have provided the
-  // tests for you in this lab to give you an example
-  // of how they should look. In the future, you will
-  // be responsible for writing your own tests.
-
-  // After you have written the code to make the tests
-  // pass, exercise your code in a driver function in
-  // App.java. This shows that your business logic can
-  // run in a console program.
+    return remaingOunces;
+  }
 }
