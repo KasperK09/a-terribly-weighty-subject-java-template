f00f67ba93ca91a3c7ec8bfba787f0d2eff89edc
Kasper <93494353+KasperK09@users.noreply.github.com>
2026-09-05 13:32:46 -0500

    started to implement the app class

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
     System.out.println("Welcome to the Ounce Conversion Program!");
-    
+    System.out.println("How many ounces do you have?");
+
+    Scanner console = new Scanner(System.in);
+
+    int ounces = console.nextInt();
+
+    Converter converter = new Converter();
+
+    System.out.println("Thank you, converting ounces to pounds...");
+
+    System.out.println(ounces + " oz is equivalent to " + pounds);
+
+
   }
 }
