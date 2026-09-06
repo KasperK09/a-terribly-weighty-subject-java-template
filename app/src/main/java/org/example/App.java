040e215724c84a258b0f922b2a13c2e4fbe2ff3a
Kasper <93494353+KasperK09@users.noreply.github.com>
2026-09-06 15:17:43 -0500

    added a comment

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -9,6 +9,7 @@ public class App {
 
     Scanner console = new Scanner(System.in);
 
+    //this breaks the program when you enter anything other than an integer so change it if a floating point value is needed
     int ounces = console.nextInt();
 
     Converter converter = new Converter();
